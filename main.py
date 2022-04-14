6aa8c1d8ff1035b3bdaa4ec952d595545378023f
hanka <hankakokocinska@wp.pl>
2022-04-14 13:15:38 +0200

    changed login to email address, added registration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import random
-from flask import Flask, get_flashed_messages, flash, session, render_template, request, redirect, json
-from werkzeug.security import check_password_hash
+from flask import Flask, get_flashed_messages, flash, session, render_template, request, redirect, json, jsonify
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'secret-key-1103'
@@ -13,6 +13,37 @@ def get_connection():
     return conn
 
 
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+
+        if request.method == 'GET':
+            messages = get_flashed_messages()
+            return render_template('registration.html', messages=messages)
+        if request.method == 'POST':
+            username = request.form['username']
+            email_address = request.form['email_address']
+            password = generate_password_hash(request.form['password'])
+
+            conn = get_connection()
+            c1 = conn.cursor()
+            result = c1.execute('SELECT * FROM users WHERE email_address = ?', (email_address,))
+            user_data = result.fetchone()
+
+            if user_data == None:
+
+                c = conn.cursor()
+                quiry = """
+                            INSERT INTO "users" ("email_address", "username", "password") VALUES (?, ?, ?)
+                            """
+                parameters = (email_address, username, password)
+                c.execute(quiry, parameters)
+                conn.commit()
+                conn.close()
+                return redirect('/')
+
+            flash('Podany użytkownik już istnieje, zaloguj się.')
+            return redirect('/registration')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -20,13 +51,13 @@ def login():
         return render_template('login.html', messages=messages)
 
     if request.method == 'POST':
-        username = request.form['username']
+        email_address = request.form['email_address']
         password = request.form['password']
 
         conn = get_connection()
         c = conn.cursor()
 
-        result = c.execute('SELECT * FROM users WHERE username = ?', (username,))
+        result = c.execute('SELECT * FROM users WHERE email_address = ?', (email_address,))
         user_data = result.fetchone()
         conn.close()
         if user_data:
@@ -34,6 +65,7 @@ def login():
 
             if check_password_hash(hashed_password, password):
                 session['user_id'] = user_data['id']
+                session['email_address'] = user_data['email_address']
                 session['username'] = user_data['username']
 
                 return render_template('welcome.html', session=session) and redirect('/')
@@ -45,6 +77,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()
+    flash('Zostałeś wylogowany')
     return redirect('/login')
 
 
@@ -60,7 +93,13 @@ def login_required(view):
 
 @app.route('/')
 def welcome():
-    return render_template('welcome.html')
+    if session:
+
+        user = session['username']
+    else:
+        user = None
+    return render_template('welcome.html', user=user)
+
 
 @app.route('/reasoning/', methods=['GET', 'POST'], endpoint='reasoning')
 @login_required
@@ -70,8 +109,9 @@ def reasoning():
 
 conn = get_connection()
 c = conn.cursor()
-query1= c.execute('SELECT * FROM "reasoning"')
+query1 = c.execute('SELECT * FROM "reasoning"')
 data1 = query1.fetchall()
+
 
 @app.route('/reasoning/questions', methods=['GET', 'POST'])
 def reasoning_questions():
@@ -80,6 +120,7 @@ def reasoning_questions():
 
     questions = dict({"reasoning": data_dict})
     return questions
+
 
 @app.route('/reasoning/answers', methods=['POST'])
 def reasoning_answers():
@@ -103,11 +144,10 @@ def reasoning_answers():
         tests_number = number[0]
 
     for i in result['qa']:
-
         quiry2 = """
                 INSERT INTO "answers_reasoning" ("id_user", "tests_number", "id_reasoning", "my_answer") VALUES (?, ?, ?, ?)
                 """
-        parameters = (id_user, (tests_number+1), i['id'], i['my_answer'])
+        parameters = (id_user, (tests_number + 1), i['id'], i['my_answer'])
         c.execute(quiry2, parameters)
 
     conn.commit()
@@ -116,16 +156,17 @@ def reasoning_answers():
     return result
 
 
-
 conn = get_connection()
 c = conn.cursor()
-query2= c.execute('SELECT * FROM "compare_items"')
+query2 = c.execute('SELECT * FROM "compare_items"')
 data2 = query2.fetchall()
+
 
 @app.route('/compare_items', methods=['GET', 'POST'], endpoint='compare_items')
 @login_required
 def compare_items():
     return render_template('compare_items.html')
+
 
 @app.route('/compare_items/questions', methods=['GET', 'POST'])
 def compare_items_questions():
@@ -134,6 +175,7 @@ def compare_items_questions():
 
     questions = dict({"compare_items": data_dict})
     return questions
+
 
 @app.route('/compare_items/answers', methods=['POST'])
 def compare_items_answers():
@@ -157,17 +199,17 @@ def compare_items_answers():
         tests_number = number[0]
 
     for i in result['qa']:
-
         quiry2 = """
                 INSERT INTO "answers_compare_items" ("id_user", "tests_number", "id_compare_items", "my_answer") VALUES (?, ?, ?, ?)
                 """
-        parameters = (id_user, (tests_number+1), i['id'], i['my_answer'])
+        parameters = (id_user, (tests_number + 1), i['id'], i['my_answer'])
         c.execute(quiry2, parameters)
 
     conn.commit()
     conn.close()
 
     return result
+
 
 @app.route('/results', endpoint='results')
 @login_required
@@ -200,13 +242,13 @@ def results():
     """
     c2.execute(quiry2, (id_user,))
 
-
     answers_compare_items = []
     for item in c2:
         answers_compare_items.append(item)
     conn.close()
 
-    return render_template('results.html', answers_reasoning=answers_reasoning, answers_compare_items=answers_compare_items)
+    return render_template('results.html', answers_reasoning=answers_reasoning,
+                           answers_compare_items=answers_compare_items)
 
 
 if __name__ == '__main__':
