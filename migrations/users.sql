eb5e4a459fcd88427121f3812ea0084060f45453
hanka <hankakokocinska@wp.pl>
2022-04-14 13:11:43 +0200

    added email_address to users.sgl

--- migrations/users.sql
+++ migrations/users.sql
@@ -3,10 +3,11 @@ CREATE TABLE "users"
 (
     "id"       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     "username" TEXT    NOT NULL,
+    "email_address" TEXT    NOT NULL UNIQUE,
     "password" TEXT    NOT NULL
 );
 
 INSERT INTO "users"
-VALUES (NULL, 'hanka', 'pbkdf2:sha256:260000$osiyy39MJeQgH1GJ$c48e4284a6ab296e1057b21ab4ecc4f711cefbb6e7a80f597f9093797233aabd');
+VALUES (NULL, 'olarobocza', 'olarobocza@wp.pl', 'pbkdf2:sha256:260000$5RX3gz8x0zYYdV0b$40b4e799bbb610afec79c931075c72aa9876861e5648599c61fdcdde641ba822');
 INSERT INTO "users"
-VALUES (NULL, 'marcin', 'pbkdf2:sha256:260000$BQMl6D8kMeORPL0M$117b75dd75d77d8427cc4123459a2620788b1becb3a08a76228316aa9ff02a34');
+VALUES (NULL, 'marcin', 'marcin@wp.pl', 'pbkdf2:sha256:260000$oYfOrddkoPhZiGHe$dbb8174fb6fc506fa7f20787090650444a00f225c9c94ad911cb15ae60936f84');
