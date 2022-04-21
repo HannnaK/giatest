b1392e60c2ef47b350ca26ea0ec137743da9abdb
hanka <hankakokocinska@wp.pl>
2022-04-21 10:59:59 +0200

    added send_email and saving of results to pdf

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import sqlite3
 import random
+import smtplib
 from flask import Flask, get_flashed_messages, flash, session, render_template, request, redirect, json, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
+from fpdf import FPDF
+from os.path import basename
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from login_mail import user, password
 
 app = Flask(__name__)
 app.secret_key = 'secret-key-1103'
@@ -12,6 +19,46 @@ def get_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+
+def results_compare_items():
+    conn = sqlite3.connect('base.db')
+    c = conn.cursor()
+    quiry2 = """
+        SELECT ci.id, ci.letter1, ci.letter2,  ci.letter3, ci.letter4, ci.letter5, ci.letter6,  ci.letter7, ci.letter8, ci.correct_answer, aci.my_answer, aci.tests_number FROM "compare_items" AS ci
+        LEFT JOIN "answers_compare_items" AS aci 
+        ON ci.id=aci.id_compare_items
+        LEFT JOIN "users" AS u
+        ON aci.id_user=u.id
+        WHERE aci.id_user=?
+        """
+    id_user = int(session['user_id'])
+    c.execute(quiry2, (id_user,))
+
+    answers_compare_items = []
+    for item in c:
+        answers_compare_items.append(item)
+    conn.close()
+    return answers_compare_items
+
+def results_reasoning():
+    conn = sqlite3.connect('base.db')
+    c = conn.cursor()
+    quiry1 = """
+    SELECT r.id, r.statement, r.query,  r.correct_answer, ar.my_answer, ar.tests_number FROM "reasoning" AS r
+    LEFT JOIN "answers_reasoning" AS ar
+    ON r.id=ar.id_reasoning
+    LEFT JOIN "users" AS u
+    ON ar.id_user=u.id
+    WHERE ar.id_user=?
+    """
+    id_user = int(session['user_id'])
+    c.execute(quiry1, (id_user,))
+
+    answers_reasoning = []
+    for questions in c:
+        answers_reasoning.append(questions)
+    conn.close()
+    return answers_reasoning
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
@@ -115,7 +162,7 @@ data1 = query1.fetchall()
 
 @app.route('/reasoning/questions', methods=['GET', 'POST'])
 def reasoning_questions():
-    # data_sufle = random.shuffle(data1)
+    data_sufle = random.shuffle(data1)
     data_dict = [dict(q) for q in data1]
 
     questions = dict({"reasoning": data_dict})
@@ -156,21 +203,20 @@ def reasoning_answers():
     return result
 
 
-conn = get_connection()
-c = conn.cursor()
-query2 = c.execute('SELECT * FROM "compare_items"')
-data2 = query2.fetchall()
-
-
 @app.route('/compare_items', methods=['GET', 'POST'], endpoint='compare_items')
 @login_required
 def compare_items():
     return render_template('compare_items.html')
 
 
+conn = get_connection()
+c = conn.cursor()
+query2 = c.execute('SELECT * FROM "compare_items"')
+data2 = query2.fetchall()
+
 @app.route('/compare_items/questions', methods=['GET', 'POST'])
 def compare_items_questions():
-    # data_sufle = random.shuffle(data)
+    data_sufle = random.shuffle(data2)
     data_dict = [dict(q) for q in data2]
 
     questions = dict({"compare_items": data_dict})
@@ -214,42 +260,56 @@ def compare_items_answers():
 @app.route('/results', endpoint='results')
 @login_required
 def results():
-    conn = sqlite3.connect('base.db')
-    c1 = conn.cursor()
-    quiry1 = """
-    SELECT r.id, r.statement, r.query,  r.correct_answer, ar.my_answer, ar.tests_number FROM "reasoning" AS r
-    LEFT JOIN "answers_reasoning" AS ar
-    ON r.id=ar.id_reasoning
-    LEFT JOIN "users" AS u
-    ON ar.id_user=u.id
-    WHERE ar.id_user=?
-    """
-    id_user = int(session['user_id'])
-    c1.execute(quiry1, (id_user,))
-
-    answers_reasoning = []
-    for questions in c1:
-        answers_reasoning.append(questions)
-
-    c2 = conn.cursor()
-    quiry2 = """
-    SELECT ci.id, ci.letter1, ci.letter2,  ci.letter3, ci.letter4, ci.letter5, ci.letter6,  ci.letter7, ci.letter8, ci.correct_answer, aci.my_answer, aci.tests_number FROM "compare_items" AS ci
-    LEFT JOIN "answers_compare_items" AS aci 
-    ON ci.id=aci.id_compare_items
-    LEFT JOIN "users" AS u
-    ON aci.id_user=u.id
-    WHERE aci.id_user=?
-    """
-    c2.execute(quiry2, (id_user,))
-
-    answers_compare_items = []
-    for item in c2:
-        answers_compare_items.append(item)
-    conn.close()
+    answers_reasoning = results_reasoning()
+    answers_compare_items = results_compare_items()
 
     return render_template('results.html', answers_reasoning=answers_reasoning,
                            answers_compare_items=answers_compare_items)
 
+@app.route('/send_email', endpoint='send_email')
+@login_required
+
+def send_email():
+    answers_compare_items = results_compare_items()
+
+    pdf_compare_items = FPDF()
+    pdf_compare_items.add_page()
+    pdf_compare_items.add_font('DejaVu', fname='DejaVuSansCondensed.ttf')
+    pdf_compare_items.set_font('DejaVu', size=14)
+    print(answers_compare_items)
+    pdf_compare_items.cell(200, 10, txt='TO jest', ln=1)
+    for aci in answers_compare_items:
+        aci = str(aci)
+        pdf_compare_items.cell(200, 10, txt=aci, ln=2)
+    pdf_compare_items.output("pdf_compare_items.pdf")
+
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Wynik testu'
+    msg['From'] = 'Testy GIA'
+    msg['To'] = 'hankakokocinska@wp.pl'
+
+
+    content = '''Witaj
+
+    W załączniku przesyłamy wyniki testów.
+
+    Miłego dnia,
+    Testy GIA'''
+    body = MIMEText(content, 'plain')
+    msg.attach(body)
+
+    compare_items_file = 'pdf_compare_items.pdf'
+    with open(compare_items_file, mode='rb') as f:
+
+        part = MIMEApplication(f.read(), Name=basename(compare_items_file))
+        part['Content-Disposition'] = 'attachment; filename="{}"'.format(basename(compare_items_file))
+    msg.attach(part)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(user, password)
+        smtp.send_message(msg)
+
+    return ('wysłany mail')
 
 if __name__ == '__main__':
     app.run(debug=True)
