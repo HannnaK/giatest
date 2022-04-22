8fdabe3acd3499a33fa9d5a590b96ef3d902f8d5
hanka <hankakokocinska@wp.pl>
2022-04-22 12:18:34 +0200

    fixed pdf view

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ def get_connection():
 def results_compare_items():
     conn = sqlite3.connect('base.db')
     c = conn.cursor()
-    quiry2 = """
+    quiry = """
         SELECT ci.id, ci.letter1, ci.letter2,  ci.letter3, ci.letter4, ci.letter5, ci.letter6,  ci.letter7, ci.letter8, ci.correct_answer, aci.my_answer, aci.tests_number FROM "compare_items" AS ci
         LEFT JOIN "answers_compare_items" AS aci 
         ON ci.id=aci.id_compare_items
@@ -20,7 +20,7 @@ def results_compare_items():
         WHERE aci.id_user=?
         """
     id_user = int(session['user_id'])
-    c.execute(quiry2, (id_user,))
+    c.execute(quiry, (id_user,))
 
     answers_compare_items = []
     for item in c:
@@ -32,8 +32,8 @@ def results_compare_items():
 def results_reasoning():
     conn = sqlite3.connect('base.db')
     c = conn.cursor()
-    quiry1 = """
-    SELECT r.id, r.statement, r.query,  r.correct_answer, ar.my_answer, ar.tests_number FROM "reasoning" AS r
+    quiry = """
+    SELECT r.statement, r.query,  r.correct_answer, ar.my_answer, ar.tests_number FROM "reasoning" AS r
     LEFT JOIN "answers_reasoning" AS ar
     ON r.id=ar.id_reasoning
     LEFT JOIN "users" AS u
@@ -41,13 +41,64 @@ def results_reasoning():
     WHERE ar.id_user=?
     """
     id_user = int(session['user_id'])
-    c.execute(quiry1, (id_user,))
+    c.execute(quiry, (id_user,))
 
     answers_reasoning = []
     for questions in c:
         answers_reasoning.append(questions)
     conn.close()
     return answers_reasoning
+
+
+def results_reasoning_pdf():
+    conn = sqlite3.connect('base.db')
+    c = conn.cursor()
+    quiry = """
+    SELECT ar.tests_number, r.statement, r.query,  r.correct_answer, ar.my_answer FROM "reasoning" AS r
+    LEFT JOIN "answers_reasoning" AS ar
+    ON r.id=ar.id_reasoning
+    LEFT JOIN "users" AS u
+    ON ar.id_user=u.id
+    WHERE ar.id_user=?
+    """
+    id_user = int(session['user_id'])
+    c.execute(quiry, (id_user,))
+
+    answers_reasoning = []
+    for questions in c:
+        answers_reasoning.append(questions)
+    conn.close()
+
+    return answers_reasoning
+
+
+def results_compare_items_pdf():
+    conn = sqlite3.connect('base.db')
+    c = conn.cursor()
+    quiry = """
+                SELECT aci.tests_number, ci.letter1, ci.letter2, ci.letter3, ci.letter4, ci.letter5, ci.letter6,  ci.letter7, ci.letter8, ci.correct_answer, aci.my_answer FROM "compare_items" AS ci
+                LEFT JOIN "answers_compare_items" AS aci 
+                ON ci.id=aci.id_compare_items
+                LEFT JOIN "users" AS u
+                ON aci.id_user=u.id
+                WHERE aci.id_user=?
+                """
+    id_user = int(session['user_id'])
+    c.execute(quiry, (id_user,))
+
+    answers_compare_items = []
+    for item in c:
+        answers_compare_item = []
+        row = ''.join(str(el) for el in item)
+        answers_compare_item.append(row[0])
+        answers_compare_item.append(row[1:5])
+        answers_compare_item.append(row[5:9])
+        answers_compare_item.append(row[9])
+        answers_compare_item.append(row[10])
+        answers_compare_items.append(answers_compare_item)
+    conn.close()
+
+    return answers_compare_items
 
 
 def login_required(view):
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import smtplib
+import sqlite3
 from flask import Flask, session, render_template
-from fpdf import FPDF
+from fpdf import FPDF, XPos, YPos
 from os.path import basename
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-from functions import results_compare_items, results_reasoning, login_required
+from functions import results_compare_items, results_reasoning, login_required, results_reasoning_pdf, \
+    results_compare_items_pdf
 from registration_login import auth_bp
 from solving_tests import solving_tests_bp
 from api_gia import api_gia_bp
@@ -41,23 +43,51 @@ def results():
 @app.route('/send_email', endpoint='send_email')
 @login_required
 def send_email():
-    answers_compare_items = results_compare_items()
+    answers_reasoning = results_reasoning_pdf()
+    answers_compare_items = results_compare_items_pdf()
 
-    pdf_compare_items = FPDF()
-    pdf_compare_items.add_page()
-    pdf_compare_items.add_font('DejaVu', fname='DejaVuSansCondensed.ttf')
-    pdf_compare_items.set_font('DejaVu', size=14)
-    print(answers_compare_items)
-    pdf_compare_items.cell(200, 10, txt='TO jest', ln=1)
-    for aci in answers_compare_items:
-        aci = str(aci)
-        pdf_compare_items.cell(200, 10, txt=aci, ln=2)
-    pdf_compare_items.output("pdf_compare_items.pdf")
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.add_font('DejaVu', fname='DejaVuSansCondensed.ttf')
+    pdf.set_font('DejaVu', size=10)
+    line_height = pdf.font_size * 2.5
+    col_width = pdf.epw / 5
+    pdf.cell(0, 0, 'TEST ROZUMOWANIE', align='C', new_x=XPos.RIGHT, new_y=YPos.TOP)
+    pdf.ln(line_height)
+    heading = ['Nr testu', 'Twierdzenie', 'Pytanie', 'Poprawna odp.', 'Twoja odp.']
+    for item in heading:
+        pdf.multi_cell(col_width, line_height, item,
+                       new_x=XPos.RIGHT, new_y=YPos.TOP, max_line_height=pdf.font_size)
+    pdf.ln(line_height)
+    for item in answers_reasoning:
+        for el in item:
+            el = str(el)
+            pdf.multi_cell(col_width, line_height, el, border=1,
+                           new_x=XPos.RIGHT, new_y=YPos.TOP, max_line_height=pdf.font_size)
+        pdf.ln(line_height)
+    pdf.ln(line_height)
+    pdf.ln(line_height)
+
+    pdf.cell(0, 0, 'TEST PORÓWNYWANIE ELEMENTÓW', align='C', new_x=XPos.RIGHT, new_y=YPos.TOP)
+    pdf.ln(line_height)
+    heading = ['Nr testu', 'Litery 1', 'Litery 2', 'Poprawna odp.', 'Twoja odp.']
+    for item in heading:
+        pdf.multi_cell(col_width, line_height, item,
+                       new_x=XPos.RIGHT, new_y=YPos.TOP, max_line_height=pdf.font_size)
+    pdf.ln(line_height)
+    for item in answers_compare_items:
+        for el in item:
+            el = str(el)
+            pdf.multi_cell(col_width, line_height, el, border=1,
+                           new_x=XPos.RIGHT, new_y=YPos.TOP, max_line_height=pdf.font_size)
+        pdf.ln(line_height)
+
+    pdf.output('pdf_results.pdf')
 
     msg = MIMEMultipart()
     msg['Subject'] = 'Wynik testu'
     msg['From'] = 'Testy GIA'
-    msg['To'] = 'hankakokocinska@wp.pl'
+    msg['To'] = session['email_address']
 
     content = '''Witaj
 
@@ -68,7 +98,7 @@ def send_email():
     body = MIMEText(content, 'plain')
     msg.attach(body)
 
-    compare_items_file = 'pdf_compare_items.pdf'
+    compare_items_file = 'pdf_results.pdf'
     with open(compare_items_file, mode='rb') as f:
         part = MIMEApplication(f.read(), Name=basename(compare_items_file))
         part['Content-Disposition'] = 'attachment; filename="{}"'.format(basename(compare_items_file))
